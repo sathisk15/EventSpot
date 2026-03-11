7c8f00f3e324b5cd7f0ff24be736bce1cc84ac30
sathisk15 <rsathisk15@gmail.com>
2026-03-11 16:04:22 +0100

    feat: initialize expo project with eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  extends: ['@react-native', 'prettier'],
+  plugins: ['react', 'react-native'],
+  rules: {
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react-native/no-inline-styles': 2,
+    'react-native/no-color-literals': 2,
+    'react-native/no-raw-text': 2,
+    'react-native/no-single-element-style-arrays': 2,
+  },
+};
